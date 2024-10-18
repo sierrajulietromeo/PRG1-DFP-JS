ade647f1e9e8798a7993cbb94ca35a91b3ac5cf3
MitchLloyd <mitchelllloyd154@gmail.com>
2024-10-18 13:35:55 +0000

    prettify tests and tighten structure

--- dfp.test.js
+++ dfp.test.js
@@ -1,111 +1,79 @@
-const { parseFile } = require('./dfp.js');
+const fs = require("fs");
+const { parseFile } = require("./dfp.js");
 
 // Run the tests by typing npm test in the terminal below
 
-describe('Data File Parser function tests', () => {
-    
-    test("T01_Parse_FunctionDeclared", async function() {
-        expect( typeof(parseFile) ).toBe('function');
-    });
+describe("Data File Parser", () => {
+  const test_output = "./testing/outputfile_test.csv";
+  beforeEach(() => {
+    if (fs.existsSync(test_output)) {
+      fs.unlinkSync(test_output); // blocking delete, remove existing file manually
+    }
+  });
 
-    test("T02_BasicOperation", async function() {
-  
-        const fs = require('fs');
-        
-        let test_input = './testing/testdata_5.csv'
-        let test_output = './testing/outputfile_test.csv'
-        let test_exported_records = 5
-        
-        // test number of rows returned as part of the parse
-        expect( parseFile( test_input, test_output ) ).toBe(test_exported_records);
-       
-    });
+  test("T01 parseFile is a function", function () {
+    expect(typeof parseFile).toBe("function");
+  });
 
-    test("T03_ExportCreated", async function() {
-        const fs = require('fs');  
-        let test_input = './testing/testdata_5.csv'
-        let test_output = './testing/outputfile_test.csv'
-        let test_exported = 5
-        
-        if (fs.existsSync(test_output)) {
-          fs.unlinkSync(test_output); // blocking delete, remove existing file manually
-        }
-         
-        expect( parseFile( test_input, test_output ) ).toBe(test_exported);
-        expect( fs.existsSync(test_output) ).toBe(true); // check output file created
+  test("T02 reads input file and returns number of rows in the file", () => {
+    const test_input = "./testing/testdata_5.csv";
+    const test_output = "./testing/outputfile_test.csv";
+    const test_exported_records = 5;
 
-        
-      
-    });
+    // test number of rows returned as part of the parse
+    expect(parseFile(test_input, test_output)).toBe(test_exported_records);
+  });
 
-    test("T04_ExportFileSize", async function() {
-        const fs = require('fs');
-        
-        let test_input = './testing/testdata_5.csv'
-        let test_output = './testing/outputfile_test.csv'
-        let test_exported = 5
-        
-        if (fs.existsSync(test_output)) {
-            fs.unlinkSync(test_output); // blocking delete, remove existing file manually
-        }
-        
-        expect( parseFile( test_input, test_output ) ).toBe( test_exported );
-        
-        if (fs.existsSync(test_output)) {
-            let size = fs.statSync(test_output).size;
-            parseFile( test_input, test_output );
-            expect(fs.statSync(test_output).size ).toBe (size);
-        } else {
-            throw 'ERROR';
-        }
-    
-    });
+  test("T03 check output file is exists after running parseFile", () => {
+    const test_input = "./testing/testdata_5.csv";
+    const test_output = "./testing/outputfile_test.csv";
+    const test_exported = 5;
 
+    expect(parseFile(test_input, test_output)).toBe(test_exported);
+    expect(fs.existsSync(test_output)).toBe(true); // check output file created
+  });
 
-    test("T05_SourceFileExists", async function() {
-        let test_input = './testing/DOESNOTEXIST.csv'
-        let test_output = './testing/outputfile_test.csv'
-        let test_exported = -1
-      
-        expect( parseFile( test_input, test_output ) ).toBe( test_exported );
-      
-      });
+  test("T04 check the size of the output file", () => {
+    const test_input = "./testing/testdata_5.csv";
+    const test_output = "./testing/outputfile_test.csv";
+    const test_exported = 5;
 
+    expect(parseFile(test_input, test_output)).toBe(test_exported);
 
-    test("T06_ExportResultsConfirmation", async function() {
-        const fs = require('fs');
-        
-        test_input = './testing/testdata_1.csv'
-        test_output = './testing/outputfile_test.csv'
-        test_output_string = 'positive;Alpha Beta Gamma Del' + '\n'
-        test_exported = 1
-        
-        if (fs.existsSync(test_output)) {
-          fs.unlinkSync(test_output); // blocking delete, remove existing file manually
-        }
-        
-        expect( parseFile( test_input, test_output ) ).toBe( test_exported );
-        expect( fs.readFileSync(test_output).toString() ).toBe( test_output_string );
-      
-      });
+    if (fs.existsSync(test_output)) {
+      const size = fs.statSync(test_output).size;
+      parseFile(test_input, test_output);
+      expect(fs.statSync(test_output).size).toBe(size);
+    } else {
+      throw "ERROR";
+    }
+  });
 
+  test("T05 when input file doesn't exist returns -1", () => {
+    const test_input = "./testing/DOESNOTEXIST.csv";
+    const test_output = "./testing/outputfile_test.csv";
+    const test_exported = -1;
 
-    test("T07_ExportResultsCommaDelimiterConfirmation", async function() {
-        const fs = require('fs');
-        
-        test_input = './testing/testdata_1_comma.csv'
-        test_output = './testing/outputfile_test.csv'
-        test_output_string = 'positive,Alpha Beta Gamma Del' + '\n'
-        test_exported = 1
-        
-        if (fs.existsSync(test_output)) {
-          fs.unlinkSync(test_output); // blocking delete, remove existing file manually
-        }
-        
-        expect( parseFile( test_input, test_output, ',' ) ).toBe( test_exported );
-        expect( fs.readFileSync(test_output).toString() ).toBe( test_output_string );
-      
-      });
+    expect(parseFile(test_input, test_output)).toBe(test_exported);
+  });
 
+  test("T06 checks the output is correctly formatted and written to output file", () => {
+    const test_input = "./testing/testdata_1.csv";
+    const test_output = "./testing/outputfile_test.csv";
+    const test_output_string = "positive;Alpha Beta Gamma Del" + "\n";
+    const test_exported = 1;
 
+    expect(parseFile(test_input, test_output)).toBe(test_exported);
+    expect(fs.readFileSync(test_output).toString()).toBe(test_output_string);
+  });
+
+  test("T07 works for files with different delimiters", () => {
+    const test_input = "./testing/testdata_1_comma.csv";
+    const test_output = "./testing/outputfile_test.csv";
+    const test_output_string = "positive,Alpha Beta Gamma Del" + "\n";
+    const test_exported = 1;
+
+    expect(parseFile(test_input, test_output, ",")).toBe(test_exported); // delimiter is set to ',' in call to parseFile
+    expect(fs.readFileSync(test_output).toString()).toBe(test_output_string);
+  });
 });
